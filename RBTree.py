4dc0e12a7bf3e14035634e1a8f84a65154fb2c48
k2ev <30880794+k2ev@users.noreply.github.com>
2017-09-08 02:56:56 -0400

    RBTree with insertion working

---
+++ RBTree.py
@@ -0,0 +1,120 @@
+from Nodes import *
+from BST import BstRegular
+import constants
+
+
+class BstRB(BstRegular):
+
+    def insert(self, val):
+        node = self.val_to_node(val)
+        node = super().insert(node)
+        if node:
+            node.color = constants.color_red
+            self.rebalance(node)
+        return node
+
+    def rebalance(self, node):
+        if node is self.root:  # node is root
+            node.color = constants.color_black
+        elif node.parent is self.root:   # node parent is a root
+            assert node.parent.color is constants.color_black
+        elif node.parent.color is constants.color_black:
+            pass
+        elif node.get_uncle() and node.get_uncle().color is constants.color_red: # node uncle is red in color
+            node.parent.color = constants.color_black
+            node.get_uncle().color = constants.color_black
+            node.get_grandparent().color = constants.color_red
+            self.rebalance(node.get_grandparent())
+        else:  # uncle black in color
+            if node.parent.is_left_child() and node.is_left_child():
+                self._right_rotate_swap(node)
+            elif node.parent.is_left_child() and node.is_right_child():
+                self._left_right_rotate_swap(node)
+            elif node.parent.is_right_child() and node.is_right_child():
+                self._left_rotate_swap(node)
+            elif node.parent.is_right_child() and node.is_left_child():
+                self._right_left_rotate_swap(node)
+        return
+
+    def _rotate(self, node, dir = "left"):
+        grand_parent = node
+        parent = node.right if dir == "left" else node.left
+        great_grand_parent = grand_parent.parent
+
+        ## left rotate g, making p as parent of g
+
+        # p in g position with ggp as parent
+        if great_grand_parent:
+            parent.parent = great_grand_parent
+            if grand_parent.is_right_child():
+                great_grand_parent.right = parent
+            else:
+                great_grand_parent.left = parent
+        else:
+            self.root = parent
+
+        # put g as a right child of p
+        grand_parent.parent = parent
+        if dir == "left":
+            grand_parent.right = parent.left
+            parent.left = grand_parent
+        else:
+            grand_parent.left = parent.right
+            parent.right = grand_parent
+
+        return
+
+    def _swap_colors(self, node_a, node_b):
+        if node_a and node_b:
+            temp = node_a.color
+            node_a.color = node_b.color
+            node_b.color = temp
+        elif node_a:
+            node_a.color = constants.color_black
+        elif node_b:
+            node_b.color = constants.color_black
+        return
+
+    # right_right_case
+    def _left_rotate_swap(self, node):
+        grand_parent = node.get_grandparent()
+        self._rotate(grand_parent, dir="left")
+        self._swap_colors(grand_parent, grand_parent.parent)
+        return
+
+    # left_left case
+    def _right_rotate_swap(self, node):
+        grand_parent = node.get_grandparent()
+        self._rotate(grand_parent, dir="right")
+        self._swap_colors(grand_parent, grand_parent.parent)
+        return
+
+    # left right case
+    def _left_right_rotate_swap(self, node):
+        parent = node.parent
+        grand_parent = node.get_grandparent()
+        self._rotate(parent, dir="left")
+        self._rotate(grand_parent, dir="right")
+        self._swap_colors(grand_parent, grand_parent.parent)
+        return
+
+    # right left case
+    def _right_left_rotate_swap(self, node):
+        parent = node.parent
+        grand_parent = node.get_grandparent()
+        self._rotate(parent, dir="right")
+        self._rotate(grand_parent, dir="left")
+        self._swap_colors(grand_parent, grand_parent.parent)
+        return
+
+    def remove(self, val):
+        node = self.val_to_node(val)
+        node = super().remove(node)
+
+    @staticmethod
+    def val_to_node(val):
+        if type(val) is NodeWithColor:
+            return val
+        else:
+            if type(val) is int or float:
+                return NodeWithColor(val)
