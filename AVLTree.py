6ca020df904a3a23645dff78ab9095cfa2ba3382
k2ev <30880794+k2ev@users.noreply.github.com>
2017-09-25 18:11:41 -0400

    AVL tree insert

--- AVLTree.py
+++ AVLTree.py
@@ -8,13 +8,23 @@ def BstAVL(BstRegular):
         super().__init()
 
     @staticmethod
-    def height(node):
+    def get_height(node):
         return node.height if node else 0
 
-    def balance(self, node):
-        return self.height(node.left) - self.height(node.right) if node else 0
+    def get_balance(self, node):
+        return self.get_height(node.left) - self.get_height(node.right) if node else 0
 
-    
+    def insert(self, val):
+        node = super().insert(val)
+        if node:
+            # do something
+            return node
+        else:
+            return None
+
+    def _avl_balancing_on_insert(node):
+        # do something
+
 
 
 
